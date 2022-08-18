1d8049c34ad60289c1db4fdf73027805c51c8414
MuhammadazizYuldoshev <yuldoshevmuhammadaziz0705@gmail.com>
2022-08-18 19:12:35 +0500

    update complicated

--- src/main/java/uz/pdp/springbootemployeecrud/controller/Controller.java
+++ src/main/java/uz/pdp/springbootemployeecrud/controller/Controller.java
@@ -2,10 +2,7 @@ package uz.pdp.springbootemployeecrud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import uz.pdp.springbootemployeecrud.model.Employee;
 import uz.pdp.springbootemployeecrud.service.EmployeeService;
 
@@ -30,6 +27,16 @@ public class Controller {
        employeeService.save(employee);
 
        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String updateForm(@PathVariable(name = "id") int id,Model model){
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employeeList",employee);
+
+        return "edit-page";
+
     }
 
 
